bf79e1fc9fa0cc84068725cc52dc82b088243cce
JoakimAa <joakimsa@hiof.no>
2020-12-07 00:57:57 +0100

    Fixed article page

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useAuthContext } from '../context/AuthProvider';
 import { list } from '../utils/articleService.js';
+import pic from '../assets/images/pic.png';
 
 const ArticlePage = () => {
   const { isAdmin, isSuperAdmin } = useAuthContext();
@@ -22,7 +23,6 @@ const ArticlePage = () => {
       setLoading(false);
     };
     fetchData();
-    
   }, []);
 
   return (
@@ -49,23 +49,23 @@ const ArticlePage = () => {
             <FilterButton>Filter</FilterButton>
           </ButtonRight>
         </FlexBoxButtons>
-        <ArticlesWrapper>
-          {console.log(articles)};
-          {articles &&
-            articles.map((article) => (
-              <Article key={article._id}>
-                <ArticleFlexRow>
-                  <ArticleLeft>
-                    <ArticleHeader>{article.title}</ArticleHeader>
-                  </ArticleLeft>
-                  <ArticleRight>
-                    <CategoryName>{article.categoryId.name}</CategoryName>
-                  </ArticleRight>
-                </ArticleFlexRow>
-                <ArticleText>{article.content}</ArticleText>
-              </Article>
-            ))}
-        </ArticlesWrapper>
+        {articles &&
+          articles.map((article) => (
+            <MarginTop key={article._id}>
+              <Image>
+                <img name="midlertidig" src={pic} alt="midlertidig" />
+              </Image>
+              <ArticleFlexRow>
+                <ArticleLeft>
+                  <ArticleHeader>{article.title}</ArticleHeader>
+                </ArticleLeft>
+                <ArticleRight>
+                  <CategoryName>{article.categoryId.name}</CategoryName>
+                </ArticleRight>
+              </ArticleFlexRow>
+              <ArticleText>{article.content}</ArticleText>
+            </MarginTop>
+          ))}
         <Footer>
           <FooterText>OrgnNr: 007 007 007</FooterText>
           <FooterText>Ig@Igror.no</FooterText>
@@ -77,7 +77,6 @@ const ArticlePage = () => {
 };
 
 export default ArticlePage;
-import pic from '../assets/images/pic.png';
 
 const Title = styled.h1`
   font-size: 2rem;
@@ -111,6 +110,10 @@ const SearchButton = styled.button`
   text-align: center;
 `;
 
+const MarginTop = styled.article`
+  margin-top: 50px;
+`;
+
 const FilterButton = styled.button`
   color: black;
   background-color: lightgray;
@@ -118,6 +121,14 @@ const FilterButton = styled.button`
   width: 150px;
   font-weight: bold;
   text-align: center;
+`;
+
+const Image = styled.div`
+  margin-top: 10px;
+  padding-right: 20px;
+  padding-bottom: 20px;
+  float: left;
+  width: 190px;
 `;
 
 const FlexBoxButtons = styled.div`
@@ -152,9 +163,7 @@ const ArticleText = styled.div`
   font-size: 15px;
 `;
 
-const ArticleRight = styled.div`
-  justify-content: flex-end;
-`;
+const ArticleRight = styled.div``;
 
 const ArticleLeft = styled.div`
   flex: 0 0 83%;
@@ -177,85 +186,12 @@ const ButtonLeft = styled.div`
 const Footer = styled.div`
   display: flex;
   justify-content: space-between;
-  max-width: 380px;
-  margin: auto;
-  padding: 50px 0 50px 0;
-  flex-direction: row;
+  max-width: 500px;
+  margin-left: 200px;
+  padding-top: 50px;
+  padding-bottom: 50px;
 `;
 
 const FooterText = styled.p`
-  font-size: 15px;
-  font-weight: 600;
+  font-size: 18px;
 `;
-
-const MarginTop = styled.div`
-  margin-top: 50px;
-`;
-
-const Image = styled.div`
-  margin-top: 10px;
-  padding-right: 20px;
-  padding-bottom: 20px;
-  float: left;
-  width: 190px;
-`;
-<<<<<<< HEAD
-=======
-
-const ArticlePage = () => {
-  const { isAdmin, isSuperAdmin } = useAuthContext();
-
-  return (
-    <div>
-      <HeaderTitle>
-        <Title>Fagartikler</Title>
-      </HeaderTitle>
-      <SideWrapper>
-        <FlexBoxButtons>
-          {isAdmin && (
-            <ButtonLeft>
-              <NewArticleButton>Ny Artikkel</NewArticleButton>
-            </ButtonLeft>
-          )}
-          {isSuperAdmin && (
-            <ButtonLeft>
-              <NewArticleButton>Ny Artikkel</NewArticleButton>
-            </ButtonLeft>
-          )}
-          <ButtonRight>
-            <SearchButton>Søk</SearchButton>
-            <FilterButton>Filter</FilterButton>
-          </ButtonRight>
-        </FlexBoxButtons>
-        <MarginTop>
-          <Image>
-            <img name="midlertidig" src={pic} alt="midlertidig" />
-          </Image>
-          <ArticleFlexRow>
-            <ArticleLeft>
-              <ArticleHeader>ArtikkelTittel</ArticleHeader>
-            </ArticleLeft>
-            <ArticleRight>
-              <CategoryName>Kategorinavn</CategoryName>
-            </ArticleRight>
-          </ArticleFlexRow>
-          <ArticleText>
-            Vi pusser opp små og mellomstore bad for privatkunder og
-            entreprenører. Vi er opptatt av god kvalitet og bruker kun de beste
-            rørleggerne i alt vi foretar oss. Vi hjelper deg med å planlegge
-            drømmebadet ditt fra A til Å! Med hjertet for faget yter vi kvalitet
-            i alle ledd for at du skal være i trygge hender.
-          </ArticleText>
-        </MarginTop>
-        <Footer>
-          <FooterText>OrgnNr: 007 007 007</FooterText>
-          <FooterText>Ig@Igror.no</FooterText>
-          <FooterText>99 00 00 00</FooterText>
-        </Footer>
-      </SideWrapper>
-    </div>
-  );
-};
-
-export default ArticlePage;
->>>>>>> 96e38cdbe0780fe1ce93b6940a95304becabf00e
