4418b31a012e2c99b46dde26d194193dda50e0b9
AleksanderSandnes <aleksander,sandnes@hotmail.com>
2020-12-10 11:59:50 +0100

    Update article.js

--- server/schemas/article.js
+++ server/schemas/article.js
@@ -1,23 +1,23 @@
 import Joi from 'joi';
 
 const ArticleValues = {
-  title: Joi.string().title().required.messages({
+  title: Joi.string().min(1).required().messages({
     'any.required': 'Tittel må fylles ut',
     'string.empty': 'Fyll ut tittel',
   }),
-  ingress: Joi.string().ingress().required.messages({
+  ingress: Joi.string().min(50).required().messages({
     'any.required': 'Ingress må fylles ut',
     'string.empty': 'Fyll ut ingress',
   }),
-  content: Joi.string().content().required.messages({
+  content: Joi.string().min(50).required().messages({
     'any.required': 'Innhold må fylles ut',
     'string.empty': 'Fyll ut innhold',
   }),
-  category: Joi.string().category().required.messages({
+  category: Joi.string().min(1).required().messages({
     'any.required': 'Kategori må fylles ut',
     'string.empty': 'Fyll ut kategori',
   }),
-  author: Joi.string().author().required.messages({
+  author: Joi.string().min(1).required().messages({
     'any.required': 'Forfatter må fylles ut',
     'string.empty': 'Fyll ut forfatter',
   }),
